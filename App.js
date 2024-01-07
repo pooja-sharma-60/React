a8f3d65e6b5002ee44eb7e8095432587b265b646
Pooja Sharma <sharmapooja4917@gmail.com>
2024-01-07 17:30:35 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,21 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello World from React!"
+// );
+
+// console.log(heading); // object
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "It is a h1 tag"),
+    React.createElement("h2", {}, "It is a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "It is a h1 tag"),
+    React.createElement("h2", {}, "It is a h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
