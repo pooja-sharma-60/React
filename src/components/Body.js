6d94c23a01bdcb927cf3aad5e331be6dc0dab286
Pooja Sharma <sharmapooja4917@gmail.com>
2024-02-02 18:13:23 +0530

    filter functionality done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,74 @@
 import RestaurantCard from "./RestaurantCard";
-import { restaurantList } from "../utils/mockData";
-import { useState } from "react";
+// import { restaurantList } from "../utils/mockData";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const data = await res.json();
+    const restaurantList =
+      data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    setListOfRestaurants(restaurantList);
+    setFilteredRestaurants(restaurantList);
+    // console.log(
+    //   data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    // );
+  };
 
   const handleFilterClick = () => {
     let filteredList = listOfRestaurants.filter(
-      (restaurant) => restaurant.data.avgRating > 4
+      (restaurant) => restaurant?.info?.avgRating > 4
     );
-    setListOfRestaurants(filteredList);
+    setFilteredRestaurants(filteredList);
+    console.log(filteredList);
   };
 
-  return (
+  const handleSearch = () => {
+    console.log(searchText);
+    const searchedRestaurants = listOfRestaurants.filter((restaurant) =>
+      restaurant?.info?.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredRestaurants(searchedRestaurants);
+    setSearchText("");
+
+    console.log(searchedRestaurants);
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
         <button className="filter-btn" onClick={handleFilterClick}>
           Top Rated Restaurants
         </button>
       </div>
       <div className="restaurant-container">
-        {listOfRestaurants.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        {filteredRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,31 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
- const Header = () => (
-  <div className="header">
-    <div className="logo-container">
-      <img className="logo" src={LOGO_URL} alt="Logo design" />
+const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
+  const handleClick = () => {
+    setBtnName((prev) => (prev === "Login" ? "Logout" : "Login"));
+    // btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+  };
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} alt="Logo design" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button className="login-btn" onClick={handleClick}>
+            {btnName}
+          </button>
+        </ul>
+      </div>
     </div>
-    <div className="nav-items">
-      <ul>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Contact Us</li>
-        <li>Cart</li>
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;
